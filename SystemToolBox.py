f167c11867f279baad63a898f80462dd5b1a240c
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-03-13 23:23:12 +0800

    add SystemToolBox.py

---
+++ SystemToolBox.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+print('======================================')
+print('        welcome to use Toolbox        ')
+print('             This is menu             ')
+print('   Here is a rundown of the options   ')
+print('[1]init toolbox    [2]update toolbox  ')
+print('[3]Reload-drivers  [4]Reload-hosts    ')
+print('[exit]exit menu                       ')
+print('======================================')
+while True:
+    try:
+        choose = int(input('your options is: '))
+    except ValueError:
+        print('please input your options again')
+        choose = input('your options is: ')
+    if choose == 1:
+        
+    elif choose == 'exit':
+        os._exit(0)
