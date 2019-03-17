2be37f5903d82b6fd75a524a5428307150d6620a
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-03-17 18:41:18 +0800

    Update SystemToolBox.py

--- SystemToolBox.py
+++ SystemToolBox.py
@@ -2,19 +2,29 @@ import subprocess
 import os
 import time
 # ================language variable begin========================
-Tips_at_the_beginning = 'Type "help" or "license" for more information.'
+Tips_at_the_beginning = 'Type "help" or command for more information.'
 # ================language variable finish========================
 print(Tips_at_the_beginning)
 while True:
     command = input('>> ')
-    if command == 'help()':
-        helps = ['input "Reload_computer_drivers()" can reload your computer drivers','input "Reload_computer_hosts_file()" can reload your computer hosts file','input "exit()" can exit System Tool Box','help()']
+    if command == 'help':
+        helps = ['input "Reload_computer_drivers" can reload your computer drivers','input "Reload_computer_hosts_file" can reload your computer hosts file','input "exit" can exit System Tool Box']
         for i in helps:
             time.sleep(0.5)
             print(i)
-    elif command == 'exit()':
+    elif command == "Reload_computer_drivers":
+        subprocess.Popen('Reload-drivers',shell=True)
+    elif command == "Reload_computer_hosts_file":
+        subprocess.Popen('Reload-hosts',shell=True)
+    elif command == 'exit':
         print('good bye!')
         os._exit(0)
+    elif command == '':
+        pass
     else:
-        if command in 'help()':
-            print('Type "help()" for interactive help')
+        if command in 'help':
+            print('Type "help" for interactive help')
+        elif command in 'Reload':
+            print('"Reload_computer_drivers" of "Reload_computer_hosts_file"')
+        elif command in 'reload':
+            print('"Reload_computer_drivers" of "Reload_computer_hosts_file"')
