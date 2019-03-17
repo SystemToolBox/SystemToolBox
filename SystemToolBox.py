130207310eb4ba759b787cd659fa18a148982bd1
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-03-17 14:58:06 +0800

    upadte

--- SystemToolBox.py
+++ SystemToolBox.py
@@ -1,21 +1,20 @@
 import subprocess
 import os
-print('======================================')
-print('        welcome to use Toolbox        ')
-print('             This is menu             ')
-print('   Here is a rundown of the options   ')
-print('[1]init toolbox    [2]update toolbox  ')
-print('[3]Reload-drivers  [4]Reload-hosts    ')
-print('[exit]exit menu                       ')
-print('======================================')
+import time
+# ================language variable begin========================
+Tips_at_the_beginning = 'Type "help" or "license" for more information.'
+# ================language variable finish========================
+print(Tips_at_the_beginning)
 while True:
-    try:
-        choose = int(input('your options is: '))
-    except ValueError:
-        print('please input your options again')
-        choose = input('your options is: ')
-    finally:
-        if choose == 1:
-            pass
-        elif choose == 'exit':
-            os._exit(0)
+    command = input('>> ')
+    if command == 'help()':
+        helps = ['input "Reload_computer_drivers()" can reload your computer drivers','input "Reload_computer_hosts_file()" can reload your computer hosts file','input "exit()" can exit System Tool Box','help()']
+        for i in helps:
+            time.sleep(0.5)
+            print(i)
+    elif command == 'exit()':
+        print('good bye!')
+        os._exit(0)
+    else:
+        if command in 'help()':
+            print('Type "help()" for interactive help')
